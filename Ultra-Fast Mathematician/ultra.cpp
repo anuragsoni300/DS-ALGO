bccccaf20c31c4659a2bb0245757020843840cd7
Anurag Soni <soni.anurag300@gmail.com>
2020-12-31 12:44:53 +0530

    sdfe

---
+++ Ultra-Fast Mathematician/ultra.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string a,b;
+	cin>>a>>b;
+	int i=0;
+	while(a[i])
+	{
+		if(a[i] == b[i])
+			cout<<'0';
+		else
+			cout<<'1';
+		i++;
+	}
+	cout<<endl;
+	return 0;
+}
+
