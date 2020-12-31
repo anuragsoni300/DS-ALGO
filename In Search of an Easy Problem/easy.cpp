da2a2645dc39214bf635d6ad6456aecc2b55944c
Anurag Soni <soni.anurag300@gmail.com>
2020-12-31 11:11:17 +0530

    sdfe

---
+++ In Search of an Easy Problem/easy.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		int temp;
+		cin>>temp;
+		if(temp == 1)
+		{
+			cout<<"HARD"<<endl;
+			exit(0);
+		}
+	}
+	cout<<"EASY"<<endl;
+	return 0;
+}
+
