6fb1d3b8d4926a28411e427ae0a34e3d40b69718
Anurag Soni <soni.anurag300@gmail.com>
2021-01-02 17:02:24 +0530

    adsf

---
+++ Vasya the Hipster/hipster.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<min(a,b)<<' ';
+	if(abs(a-b) < 2)
+		cout<<'0'<<endl;
+	else
+		cout<< abs(a-b)/2<<endl;
+	return 0;
+}
+
