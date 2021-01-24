a705e12b00978f36b16321dd975fd3a578626a07
Anurag Soni <soni.anurag300@gmail.com>
2021-01-24 17:07:17 +0530

    asdf

---
+++ 1372A/comp.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		int a;
+		cin>>a;
+		for(int i=0;i<a;i++)
+		{
+			cout<<'1'<<' ';
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
