4f7fe5d1052d4f82964f85da99343ab4b61c6887
Anurag Soni <soni.anurag300@gmail.com>
2021-01-24 15:57:37 +0530

    asdf

---
+++ 1454A/special.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	while(n--)
+	{
+        	int a;
+		cin>>a;
+		for(int i=2;i<=a;i++)
+			cout<<i<<' ';
+		cout<<1<<endl;
+	}
+	return 0;
+}
+
